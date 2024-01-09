618d6f40c831e3b555f13294c73ba382a5ed92b3
Toysin <toysin1203@hotmail.com>
2024-01-09 14:00:03 +0900

    client: 환경변수 오타 수정 및 리전 설정

--- client/src/main/java/study/donshin/S3ClientConfig.java
+++ client/src/main/java/study/donshin/S3ClientConfig.java
@@ -4,20 +4,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3ClientConfig {
 
-    @Value("${env.aws.s3.accessKey}")
+    @Value("${env.aws.s3.credentials.accessKey}")
     private String accessKey;
 
-    @Value("${env.aws.s3.secretKey}")
+    @Value("${env.aws.s3.credentials.secretKey}")
     private String secretKey;
 
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
+                .region(Region.AP_NORTHEAST_2)
                 .credentialsProvider(() -> {
                     return AwsBasicCredentials.create(accessKey, secretKey);
                 })
